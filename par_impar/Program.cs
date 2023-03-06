feba4eff7b78466fe960086d21ce78fa52a19124
Matheus7k <06matheus.oliveira@gmail.com>
2023-03-05 23:47:02 -0300

    Add project files.

---
+++ par_impar/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static string ParOuImpar(float numero)
+    {
+        if (numero % 2 == 0)
+        {
+            return $"O numero {numero} é par";
+        }
+        else return $"O numero {numero} é impar";
+    }
+    private static void Main(string[] args)
+    {
+        float n1;
+        Console.Write("Informe o numero: ");
+        n1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine(ParOuImpar(n1));
+    }
+}
